997a7514fd234a49818b1db8b2dde65381a55ed4
rinatovich <mvrinatovicso@gmail.com>
2020-12-11 12:09:04 +0500

    v1.1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 window.addEventListener('load',()=>{
 
+
+/************Анимация перехода категорий************/
 	let categories = document.querySelectorAll('.category');
 	let activeCategory = 'active__category';
 
@@ -19,20 +21,32 @@ window.addEventListener('load',()=>{
 			categories[i].classList.add(activeCategory);
 		})
 	}
+/************Анимация перехода категорий************/
 
 
+	
+/***********Анимация перехода между вкладками**********/
 	let activeTabIndex = 0;
-
-
-
-
 	let tabItem = document.querySelectorAll('.tabs__item');
 	let tabActive = 'active__tab'; 
+	let tabs = document.querySelector('.tabs');
+
+
+
 	let chats = document.querySelectorAll('.chats');
 	let general__chats = document.querySelector('.general__chats');
 	let friends__chats = document.querySelector('.friends__chats');
 	let feedback__chats = document.querySelector('.friends__chats');
 	let active_chat = 'active__chat';
+
+	if(document.body.clientWidth <=650)
+	{
+		tabItem[0].querySelector(".tab__title").innerText = 'Чаты';
+		if(tabItem[2]!=undefined)
+		{
+			tabItem[2].querySelector(".tab__title").innerText = 'Под-ка';
+		}
+	}
 
 
 
@@ -65,6 +79,47 @@ window.addEventListener('load',()=>{
 			activeTabIndex = i;
 		})
 	}
+/***********Анимация перехода между вкладками**********/
+
+
+
+
+let animatioForTypingIndicator = function(typing, name)
+{
+	let typing__indicator = document.querySelector('.typing__indicator');
+	typing__indicator.innerText = `Пишет ${name} `;	
+	let i = 0;
+
+	if(typing == true)
+	{
+
+		setInterval(()=>{
+			typing__indicator.innerText += '.';
+			i++;
+			if(i==4)
+			{
+				i=0;
+				typing__indicator.innerText = `Пишет ${name} `;	
+			}
+		},500)
+	}
+}	
+let typing = false;
+animatioForTypingIndicator(typing,"Timur");
+
+
+
+/***********Анимация перехода между пользователями**********/
+	/*************Элементы пользователя которые отображаються в правом верхнем углу мессенджера**********/
+	let currentUserElements = 
+	{
+		name: document.querySelector('#user__name'),
+		nick: document.querySelector('.user__nick'),
+		last__seen: document.querySelector('.last__seen')
+	}
+	
+	/****************************************************************************************************/
+
 
 
 
@@ -96,6 +151,27 @@ window.addEventListener('load',()=>{
 	}
 
 
+	if(document.body.clientWidth <= 768)
+	{
+		let contract__bar__spans = document.querySelectorAll('.contract__status_bar__text');
+		let payemnt__status = document.querySelector('.payment__status__value');
+		let payment__btn = document.querySelectorAll('.pay__btn');
+
+		if(payment__btn!=undefined)
+		{	
+			payment__btn[0].style.display = 'none'
+
+
+
+			contract__bar__spans[0].innerText = 'до';
+			contract__bar__spans[1].style.display = 'none';
+			contract__bar__spans[2].style.display = 'none'; 
+
+		}	
+	}
+
+
+
 
 	let companionsList__subheader = document.querySelector('.companionsList__subheader');
 	let messenger__subheader = document.querySelector('.messenger__subheader');
@@ -114,85 +190,44 @@ window.addEventListener('load',()=>{
 						activeTabIndex = j;
 					}
 				}
-				
-
-				companions__list[activeTabIndex].classList.add('companions__list_opacity');
-				setTimeout(()=>{
-					companions__list[activeTabIndex].classList.add('display__none');
-					tab.classList.add('display__none');
-				},150)
-				setTimeout(()=>{messageplace[activeTabIndex].classList.add('display__block');},180);
-				setTimeout(()=>{messageplace[activeTabIndex].classList.add('animationMessagePlace');},200)
-
-				mobTabs.classList.add('display__none');
-				messenger__subheader.classList.add('display__block');
-				companionsList__subheader.classList.add('display__none');
-
-
-
+			companions__list[activeTabIndex].style.display ='none';
+			tab.style.display = 'none';
+			messageplace[0].style.display = 'block';
+			mobCategories.style.display = 'none';
+			companionsList__subheader.style.display = 'none';
+			messenger__subheader.style.display = 'block';
 			});
 		}
+		messenger__subheader.onclick = function(){
+			companions__list[activeTabIndex].style.display ='block';
+			tab.style.display = 'block';
+			messageplace[0].style.display = 'none';
+			mobCategories.style.display = 'block';
+			companionsList__subheader.style.display = 'block';
+			messenger__subheader.style.display = 'none';
+		}
 
-		messenger__subheader.addEventListener('click', ()=>{
-			messageplace[activeTabIndex].classList.remove('animationMessagePlace');
-			setTimeout(()=>{messageplace[activeTabIndex].classList.remove('display__block');},50);
-			
-			messageplace[activeTabIndex].classList.remove('animationMessagePlace');
-			setTimeout(()=>{messageplace[activeTabIndex].classList.remove('display__block');},50);
-
-			setTimeout(()=>{companions__list[activeTabIndex].classList.remove('display__none');},70);
-			tab.classList.remove('display__none');
-			companions__list[activeTabIndex].classList.remove('companions__list_opacity');
-
-			
-			mobTabs.classList.remove('display__none');
-			messenger__subheader.classList.remove('display__block');
-
-			companionsList__subheader.classList.remove('display__none');
-
-		});
-
-
-
-
-		let contract__bar__spans = document.querySelectorAll('.contract__status_bar__text');
-		let payemnt__status = document.querySelector('.payment__status__value');
-		let payment__btn = document.querySelector('.pay__btn');
-
-		if(payment__btn!=undefined)
-		{	
-			payment__btn.innerText = 'Оплатить';
-			payment__btn.style.width = '100px';
-
-			payemnt__status.style.display = 'none';
-
-
-			contract__bar__spans[0].innerText = 'до';
-			contract__bar__spans[1].style.display = 'none';
-			contract__bar__spans[2].style.display = 'none'; 
-
-		}		
+/***********Анимация перехода между пользователями**********/
+	
 	}
 
 
 
-	let mobTabs = document.querySelector('.mobile__categories');
+	let mobCategories = document.querySelector('.mobile__categories');
 	let mobCategory = document.querySelectorAll('.mob__category');
 	let mobCategoryChevron = document.querySelector('.categoryChevron');
 
-	mobTabs.addEventListener('click', ()=>{
-		if(mobTabs.classList.contains("activeDropdown")){
-			mobTabs.classList.remove('activeDropdown');
+	mobCategories.addEventListener('click', ()=>{
+		if(mobCategories.classList.contains("activeDropdown")){
+			mobCategories.classList.remove('activeDropdown');
 			mobCategoryChevron.classList.remove('ChevronAnimation');
 		}
 		else{
 			mobCategoryChevron.classList.add('ChevronAnimation');
-			mobTabs.classList.add('activeDropdown');
+			mobCategories.classList.add('activeDropdown');
 		}
 	})
 
-
-	let mobCategories = document.querySelectorAll('.mob__category');
 
 
 })
